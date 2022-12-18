107ff688156bab5654ec676bfa3c5545fdcd59a0
hyun <hyun.choi499@gmail.com>
2022-12-18 16:04:33 +0900

    setNull vs setString(index,null)

---
+++ src/test/java/mysql/datatype/varchar/setStringTest.java
@@ -0,0 +1,31 @@
+package mysql.datatype.varchar;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Types;
+
+import study.connection.Dbms;
+
+//CREATE TABLE nullable_varchar(vc VARCHAR(5));
+public class setStringTest {
+
+	public static void main(String[] args) throws SQLException {
+		Dbms mysql = Dbms.LOCAL_MYSQL;
+		Connection con = mysql.getTestConnection();
+
+		PreparedStatement pstmt = con.prepareStatement("insert into nullable_varchar(vc) values(?)");
+		// setNull대신 setString(번호,null) 에러발생x
+		pstmt.setString(1, null);
+		System.out.println(pstmt.executeUpdate());
+
+		pstmt.clearParameters();
+
+		pstmt.setNull(1, Types.VARCHAR);
+		System.out.println(pstmt.executeUpdate());
+
+		pstmt.close();
+		con.close();
+	}
+
+}
